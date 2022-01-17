d51cffbca2d27d2a5670f5d9a1bea3ce34b8b40c
ibrahimerkenov <44646383+ibrahimerkenov@users.noreply.github.com>
2022-01-17 21:33:57 +0300

    Updates

---
+++ static/js/main.ac5f62a6.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpweather=this.webpackJsonpweather||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);n(0);var r=n(3),c=n.n(r),s=(n(8),n(1)),i=function(){return Object(s.jsx)("div",{children:"DIIIV"})};c.a.render(Object(s.jsx)(i,{}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.ac5f62a6.chunk.js.map
